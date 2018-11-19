16469cf24fa6942bd69b5adfecce17584b94a99f
Aurèle <aurele.barriere@gmail.com>
2018-11-19 22:40:25 +0100

    few tests for ctls

--- main.ml
+++ main.ml
@@ -9,21 +9,35 @@ open Ctlskd_mc
 
 
 (* LTL Example *)
-let ltl_kripke: kripke  = [(1,[2;3]);(2,[]);(3,[])]
-let ltl_marking1: marking = [(1,[2])]
-let ltl_marking2: marking = [(1,[2;3])]
-let ltl_init: state = 1
+let kripke1: kripke  = [(1,[2;3]);(2,[]);(3,[])]
+let marking1: marking = [(1,[2])]
+let marking2: marking = [(1,[2;3])]
+let init: state = 1
 let ltl_spec: ltl = LTL_X (LTL_AP 1)
 
-(* let _ = print_string (nusmv_write_pbm ltl_kripke ltl_init ltl_marking1 ltl_spec) *)
+let ltl_1 = ltl_mc kripke1 init marking1 ltl_spec
+let ltl_2 = ltl_mc kripke1 init marking2 ltl_spec
 
-let bool1 = ltl_mc ltl_kripke ltl_init ltl_marking1 ltl_spec
+let ctl_spec1 = ST_CTLS_A (P_CTLS_X (P_CTLS_S (ST_CTLS_AP 1)))
+let ctl_spec2 = ST_CTLS_E (P_CTLS_X (P_CTLS_S (ST_CTLS_AP 1)))
 
-let bool2 = ltl_mc ltl_kripke ltl_init ltl_marking2 ltl_spec
+let ctls_1 = ctls_mc kripke1 init marking1 ctl_spec1
+let ctls_2 = ctls_mc kripke1 init marking1 ctl_spec2
 
-let _ = match bool1 with
+let _ = print_string "LTL Tests\n"
+                   
+let _ = match ltl_1 with
   | true -> print_string "first test failed\n"
   | false -> print_string "first test passed\n"
-let _ = match bool2 with
+let _ = match ltl_2 with
+  | true -> print_string "second test passed\n"
+  | false -> print_string "second test failed\n"
+
+let _ = print_string "CTLS Tests\n"
+                   
+let _ = match ctls_1 with
+  | true -> print_string "first test failed\n"
+  | false -> print_string "first test passed\n"
+let _ = match ctls_2 with
   | true -> print_string "second test passed\n"
   | false -> print_string "second test failed\n"
