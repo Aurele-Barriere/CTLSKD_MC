cd9e12edf248af2966eb0ef9ba1620db7a083ae2
Aurèle <aurele.barriere@gmail.com>
2018-11-21 10:15:54 +0100

    standard types. temporal update

--- ctlskd_mc.ml
+++ ctlskd_mc.ml
@@ -12,20 +12,24 @@ let rec append_no_dup (l1:'a list) (l2:' a list): 'a list =
   | x::l -> match List.mem x l2 with
             | true -> append_no_dup l l2
             | false -> x::(append_no_dup l l2)
+
+(* Successors of an information set *)
+let rec build_successors (i:inf_set) (k:std_kripke): inf_set =
+  match i with
+  | [] -> []
+  | x::l -> append_no_dup (successors x k) (build_successors l k)
        
 (* Delta Update UD *)
 let ud (s:std_state) (i:inf_set) (o:observation) (om:obs_marking): inf_set =
   List.filter (fun (x:std_state) -> eq_state om o s x) i
-
+              
 (* Temporal Update UT *)
-let ut (s:std_state) (i:inf_set) (o:observation) (om:obs_marking) (states:std_state list): inf_set =             
-  []                            (* TODO *)
+let ut (s:std_state) (i:inf_set) (o:observation) (om:obs_marking) (k:std_kripke): inf_set =
+  List.filter (fun (x:std_state) -> eq_state om o s x) (build_successors i k)
 
 (* Initial information Set *)
 let ii (s:std_state) (o:observation) (om:obs_marking) (states:std_state list): inf_set =
   List.filter (fun (x:std_state) -> eq_state om o s x) states
-                                                                 
-       
        
 (* CTL*KD model-Checking. Takes a model, an initial state, a marking and a specification *)
 let ctlskd_mc (k:kripke) (state_init:state) (m:marking) (obs_init:observation) (om:obs_marking) (spec:history_ctlskd): bool = true
--- models.ml
+++ models.ml
@@ -6,37 +6,36 @@ open List
 (* A Kripke Structure is a liste of states, each associated with a list of successors *)
 type kripke = (state * state list) list
 
+(* Kripke structures with standard states. Input of CTLSKD problems *)
+type std_kripke = (std_state * std_state list) list
+
 (* A marking is a list of atomic propositions, each associated with a list of states where it's true *)
 type marking = (atp * state list) list
 
-(* Returns the states of the model *)
-let get_states (k:kripke): state list = map fst k
+(* A marking for standards Kripke Structure *)
+type std_marking = (atp * std_state list) list
 
-(* Returns the standard states of a standard kripke model *)
-let rec get_std_states (k:kripke): std_state list =
-  match k with
-  | [] -> []
-  | (I x,_)::k' -> x :: (get_std_states k')
-  | _ -> failwith "get_std_states should only be called on kripke models with standard states"
+(* Returns the states of the kripke structure *)
+let get_states k = map fst k
                               
 (* Successors of a state in a model *)
-let rec successors (s:state) (k:kripke): state list =
+let rec successors s k =
   match k with
   | [] -> []
   | (s1,l)::k' when s1 = s -> l
   | (s1,l)::k' -> successors s k'
 
 (* Returns all atomic propositions of a marking *)
-let get_atp (m:marking): atp list = map fst m
+let get_atp m: atp list = map fst m
 
 (* Returns fresh atomic proposition *)
-let rec fresh_atp_rec (a:atp) (m:marking): atp =
+let rec fresh_atp_rec (a:atp) m: atp =
   match m with
   | [] -> a + 1
   | (a1,l)::m1 when a1 > a -> fresh_atp_rec a1 m1
   | (a1,l)::m1 -> fresh_atp_rec a m1
 
-let fresh_atp (m:marking): atp = fresh_atp_rec 0 m
+let fresh_atp m: atp = fresh_atp_rec 0 m
 
 (* A map from observation and (standard) states to values *)
 module ObsMap =
